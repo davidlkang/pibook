abc8c3330886d9a47e1622d0ce58e537ae9707e8
David Kang <david.kang@outlook.de>
2018-12-13 23:22:28 +0100

    added clearable list

--- public/script.js
+++ public/script.js
@@ -1,15 +1,52 @@
-window.onload = function(){
-  const button = document.getElementById("button")
-  button.onclick = function(){
-    const input = document.getElementById("input")
-    const payload = {
-      ingredient: input.value,
-    }
+let items = []
 
-    fetch("/todos", {
-      body: JSON.stringify(payload),
-      method: "POST",
-    })
-  }
+window.onload = function(){
+
+  loadList().then( () => {
+
+      const submitButton = document.getElementById("submitButton")
+      submitButton.onclick = function() {
+        const input = document.getElementById("input")
+        const payload = items.concat([input.value])
+
+        fetch("/todos", {
+          body: JSON.stringify(payload),
+          method: "POST",
+        }).then( () => {
+          input.value = ""
+          loadList()
+        })
+      }
+
+      const clearButton = document.getElementById("clearButton")
+      clearButton.onclick = clearList
+  })
 }
 
+function loadList() {
+  return fetch("/todos").then(
+    response => response.json()
+  ).then(
+    payload => {
+      items = payload
+      const list = document.getElementById("list")
+      while (list.firstChild) {
+        list.removeChild(list.firstChild)
+      }
+      items.forEach(element => {
+        const item = document.createElement("li")
+        item.innerHTML = element
+        list.appendChild(item)
+      });
+    }
+  )
+}
+
+function clearList() {
+  fetch("/todos", {
+    body: JSON.stringify([]),
+    method: "POST",
+  }).then( () => {
+    loadList()
+  })
+}
