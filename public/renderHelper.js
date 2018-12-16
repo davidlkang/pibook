9c9f2eb6a9fbc1e21797f1a675575871933e9555
David Kang <david.kang@outlook.de>
2018-12-16 02:14:53 +0100

    move delete button to items

--- public/renderHelper.js
+++ public/renderHelper.js
@@ -14,13 +14,33 @@ function buildItem(props){
   return (
     createElement('div', {
       className: "card",
-      children: [createElement('div', {
-        className: "card-content",
-        children: [
-          createElement('h4', {innerHTML: props.title}),
-          createElement('p', {innerHTML: props.description}),
-          createElement('p', {innerHTML: props.recipe}),
-        ]
+      children: [
+        createElement('div', {
+          className: "card-content",
+          children: [
+            createElement('div', {
+              className: "item-flexcontainer",
+              children: [
+                createElement('div', {
+                  className: "item-flexitem",
+                  children:[
+                    createElement('h4', {innerHTML: props.title}),
+                    createElement('p', {innerHTML: props.description}),
+                    createElement('p', {innerHTML: props.recipe}),
+                  ]
+                }),
+                createElement('div', {
+                  className: "item-flexitem",
+                  children:[
+                    createElement('button', {
+                      className: "right btn waves-effect waves-dark red darken-3",
+                      innerHTML:"delete",
+                      onclick: () => {}
+                    })
+                ]
+              })
+            ]
+        })]
       })]
     })
   )
